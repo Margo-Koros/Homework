9e84b7ceea9541398385db81d2c9f2e3f940a91f
Margaret <potapova.m.s17@gmail.com>
2022-11-21 14:36:39 +0300

    домашнее задание к третьему семинару, задача 27

---
+++ Homework4.2/Задача27.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine ("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int n1 = num/100;
+int n2 = (num/10)%10;
+int n3 = num%10;
+
+int sum = n1+n2+n3;
+Console.WriteLine (sum);

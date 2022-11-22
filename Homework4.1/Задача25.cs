687a949505f57ac97be8a1733b7b2e1ae7c1d55c
Margaret <potapova.m.s17@gmail.com>
2022-11-22 10:46:36 +0300

    домашнее задание к третьему семинару, задача 25 через цикл

--- Homework4.1/Задача25.cs
+++ Homework4.1/Задача25.cs
@@ -3,7 +3,7 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
-
+/*
 Console.WriteLine ("Введите число А ");
 int a = Convert.ToInt32(Console.ReadLine());
 
@@ -12,3 +12,22 @@ int b = Convert.ToInt32(Console.ReadLine());
 
 double c = Math.Pow(a, b);
 Console.WriteLine (c);
+*/
+
+Console.WriteLine("Введите А"); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите B"); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+ 
+Console.Write(stepen1(a, b));
+
+int stepen1 (int a, int b)
+{ 
+    int temp = 1; 
+    for (int i = 0; i<b; i++)
+    { 
+        temp *= a; 
+    }return temp; 
+}
+
+

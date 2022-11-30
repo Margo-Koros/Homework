fc51652344ca8997b1cfb6bbfeb65eb691b1b960
Margaret <potapova.m.s17@gmail.com>
2022-11-30 17:09:42 +0300

    домашнее задание 5, задача 38

---
+++ Hoomework5.3/Задача38.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double [] array = new double [8];
+Random rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0, 100);
+    Console.Write ($"{array[i]}, ");
+}
+
+    double max = array[0];
+    double min = array[0];
+
+    foreach (int a in array)
+    {
+        if (a > max)
+        {
+        // найден больший элемент
+        max = a;
+        }
+        else 
+            if (a < min)
+            {
+            // найден меньший элемент
+            min = a;
+            }
+
+    }
+Console.WriteLine ();
+Console.WriteLine (max);
+Console.WriteLine ();
+Console.WriteLine (min);
+Console.WriteLine ();
+Console.WriteLine (max - min);
+
